c117193f34d556bc65ac8ec2ecd59590fa93ee28
Vitaly <mickelangello45@gmail.com>
2018-11-28 20:46:00 +0200

    Add project files.

---
+++ UniversityStructure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
